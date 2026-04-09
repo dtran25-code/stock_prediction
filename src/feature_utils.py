b6963a4509b30476a9c3444f56767d7bc74955d2
dtran25-code <dtran25.off@gmail.com>
2026-04-09 17:26:34 -0500

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -101,7 +101,7 @@ def convert_input_pca_regression(request_body, request_content_type):
 
     if option == 2:
 
-        X = FeatureEngineer(windows=[10,15]).transform(dataset[[target]])
+        X = FeatureEngineer(windows=[5,10,15,20,25,30]).transform(dataset[[target]])
     
         techIndicator_1 = 'EMA_25'
         EMA_25 = json.loads(request_body)[techIndicator_1]
